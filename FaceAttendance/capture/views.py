f3361568a8fcf40336191d0612ba8f01c3024cb8
Prasannnnn <prasannamb4362@gmail.com>
2024-07-27 12:29:51 +0530

    views.py

--- FaceAttendance/capture/views.py
+++ FaceAttendance/capture/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PersonForm
 
-# Create your views here.
+def capture_image(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = PersonForm()
+    return render(request, 'capture_image.html', {'form': form})
+
+def success(request):
+    return render(request, 'success.html')
